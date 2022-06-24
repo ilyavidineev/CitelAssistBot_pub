aba96dcb0ee7811e6e28271ed3190a38653da95e
Ilya Vidineev <vidineev1@gmail.com>
2022-06-24 13:19:11 +0300

    Added commenting prefixes in file and its correct ignoring.

--- src/main/java/ru/vidineev/service/CSVFileService.java
+++ src/main/java/ru/vidineev/service/CSVFileService.java
@@ -21,10 +21,11 @@ import static ru.vidineev.constants.Constants.CSV_ARGS_START_INDEX;
 @Service
 public class CSVFileService {
 
-    @Value("${CSV.filepath}")
-    private String filePath;
+//    @Value("${CSV.filepath}")
+    private final String filePath = "AC_SPD_ALGO.csv"; //TODO
 
     final String SEPARATOR = ";";
+    final String COMMENT_PREFIX = "##";
 
     private static final Logger log = Logger.getLogger(CoreBot.class);
 
@@ -44,16 +45,17 @@ public class CSVFileService {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            if (!rawLine.startsWith(COMMENT_PREFIX)) {
+                String[] recEntry = rawLine.split(SEPARATOR);
 
-            String[] recEntry = rawLine.split(SEPARATOR);
+                String number = recEntry[0];
+                String method = recEntry[1];
+                String text = recEntry[2];
 
-            String number = recEntry[0];
-            String method = recEntry[1];
-            String text = recEntry[2];
-
-            List<String> listOfInlineAnswers = new ArrayList<>(asList(recEntry).subList(CSV_ARGS_START_INDEX, recEntry.length));
-            RecordString recordString = new RecordString(number, method, text, listOfInlineAnswers);
-            records.add(recordString);
+                List<String> listOfInlineAnswers = new ArrayList<>(asList(recEntry).subList(CSV_ARGS_START_INDEX, recEntry.length));
+                RecordString recordString = new RecordString(number, method, text, listOfInlineAnswers);
+                records.add(recordString);
+            }
         }
         breader.close();
 
@@ -71,12 +73,16 @@ public class CSVFileService {
         try {
             if (codeLocation.endsWith(".jar")) {
                 //Call from jar
+                log.info("Call from jar detected");
                 Path path = Paths.get(location.toURI()).resolve("../" + relativePath).normalize();
+                log.info("path: " + path.toString());
                 file = path.toFile();
 
             } else {
                 //Call from IDE
+                log.info("Call from IDE detected");
                 file = new File(CoreBot.class.getClassLoader().getResource(relativePath).getPath());
+                log.info("File: " + CoreBot.class.getClassLoader().getResource(relativePath).getPath());
             }
         } catch (URISyntaxException ex) {
             ex.printStackTrace();
